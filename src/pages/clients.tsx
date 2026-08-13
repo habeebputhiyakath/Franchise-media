7c8d52d70e7c8444ee3f89b45c5c1a710e8f2425
habeebputhiyakath <irfanputhiyakath@gmail.com>
2026-08-13 16:35:26 +0530

    feat: add PartnerLogos component and clients page with auto-scrolling logo gallery 058

--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -57,8 +57,9 @@ const partners = [
                 { name: "classic", logo: "/partner/052.png", height: "h-16" },
                 {name:"ffc" , logo:"/partner/053.png" ,height:"h-14"},
                                 {name:"yaghma" , logo:"/partner/054.png" ,height:"h-14"},
-                                {name:"broasto ",logo:"/partner/056.png",height:"h-14"},
-                                                                {name:"mandhi stories ",logo:"/partner/057.png",height:"h-18"},
+  { name: "broasto ", logo: "/partner/056.png", height: "h-14" },
+  { name: "mandhi stories ", logo: "/partner/057.png", height: "h-18" },
+  { name: "Phone Tech Education International", logo: "/partner/058.jpg", height: "h-18" },
 
 
 
